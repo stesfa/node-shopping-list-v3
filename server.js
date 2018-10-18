dda11df85c2f2ae00aeb99414fe1b24c08cba6e2
Simon Tesfamariam <stesfa@gmail.com>
2018-10-18 00:42:15 -0400

    added delete recipes

--- server.js
+++ server.js
@@ -29,6 +29,7 @@ Recipes.create(
 // all current ShoppingList items
 app.get('/shopping-list', (req, res) => {
   res.json(ShoppingList.get());
+  concole.log(req.params)
 });
 
 app.post('/shopping-list', jsonParser, (req, res) => {
@@ -53,6 +54,11 @@ app.delete('/shopping-list/:id', (req, res) => {
   res.status(204).end();
 });
 
+app.delete('/recipes/:id', (req, res) => {
+  Recipes.delete(req.params.id);
+  console.log(`Deleted recipe item \`${req.params.id}\``);
+  res.status(204).end();
+});
 
 // when new recipe added, ensure has required fields. if not,
 // log error and return 400 status code with hepful message.
